9e1613240d0143025e2366ef3df588334ca284a2
Ярослав Фролов <frolov.ya@office.lan>
2022-05-24 14:37:30 +0300

    mobile version end

--- app/js/app.js
+++ app/js/app.js
@@ -3,38 +3,69 @@ import Swiper from 'swiper/bundle'
 
 document.addEventListener('DOMContentLoaded', () => {
 	"use strict"
+	const breakpoint = window.matchMedia( '(min-width:768px)' );
 
-	const swiper = new Swiper('.swiper', {
-		loop: true,
-		slidesPerView: 1.2,
-		spaceBetween: 30,
-
-		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-		},
-
-		// Navigation arrows
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-
-		// Responsive breakpoints
-		breakpoints: {
-			// when window width is >= 768px
-			768: {
-				slidesPerView: 3,
-				spaceBetween: 20
-			},
+	// keep track of swiper instances to destroy later
+	let mySwiper;
+  
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+  
+	const breakpointChecker = function() {
+  
+	  // if larger viewport and multi-row layout needed
+	  if ( breakpoint.matches === true ) {
+  
+		// clean up old instances and inline styles when available
+		if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+  
+		// or/and do nothing
+		return;
+  
+		// else if a small viewport and single column layout needed
+		} else if ( breakpoint.matches === false ) {
+  
+		  // fire small viewport version of swiper
+		  return enableSwiper();
+  
 		}
-	});
+  
+	};
+	
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+  
+	const enableSwiper = function() {
+  
+		let swiper = new Swiper ('.swiper', {
+  
+		loop: true,
+		slidesPerView: 'auto',
+		spaceBetween: 30,
+		centeredSlides: true,
+		a11y: true,
+		keyboardControl: true,
+		grabCursor: true,
+	  });
+  
+	};
+  
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+  
+	// keep an eye on viewport size changes
+	breakpoint.addListener(breakpointChecker);
+  
+	// kickstart
+	breakpointChecker();
 })
 
 document.querySelectorAll('.about__picture-item').forEach(item => {
 	
 	item.addEventListener('click', event => {
-	  console.log(event.target);
 	  document.querySelectorAll('.about__picture_main-item')[0].classList.remove("about__picture_main-item");
 	  event.target.classList.add("about__picture_main-item");
 	  
